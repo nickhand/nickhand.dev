6fb236fc5775b8c8b327d3c0c500e9dfb4342c1f
Nick Hand <nicholas.adam.hand@gmail.com>
2022-07-11 00:08:17 -0400

    fix header spacing

--- src/sections/Header.vue
+++ src/sections/Header.vue
@@ -4,13 +4,13 @@ import SocialIcons from "../components/SocialIcons.vue";
 </script>
 
 <template>
-  <div class="flex-col-center h-screen bg-gradient-to-b from-[#25cef7]">
+  <div class="flex-col-center h-screen bg-gradient-to-b from-[#25cef7] px-0.5">
     <!-- Name header -->
     <h1 class="text-7xl font-bold text-center">NICK HAND, PHD</h1>
 
     <!-- Sub-header: typewriter for jobs -->
     <TypeWriter
-      class="text-6xl mt-5 mb-20 text-center"
+      class="text-6xl mt-5 mb-10 text-center min-h-[140px]"
       highlight-color="#FF4908"
       :display-text-array="[
         'Public Servant',
